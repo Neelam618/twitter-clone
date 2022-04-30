5fc2f21f830dcd118df04dad13098ce17f8a322b
Neelam Chavan <neelamchavan410@gmail.com>
2022-04-30 11:32:35 +0530

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import SideNav from "./components/layout/sidenav/SideNav";
+import Main from "./components/layout/main/Main";
+import Aside from "./components/layout/aside/Aside";
 
 function App() {
   return (
     <div>
-
+      <SideNav />
+      <Main />
+      <Aside />
     </div>
   );
 }
